4eaa99b72d5a70eb996b265bf2731366c95cc5e7
gagaswin <gagaswahyuin@gmail.com>
2025-01-31 15:39:29 +0700

    initialize git

---
+++ src/test/java/com/sims/ppob/PpobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sims.ppob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
